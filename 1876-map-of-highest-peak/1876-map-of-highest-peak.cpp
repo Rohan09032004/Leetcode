617ad2ab1e914461db3158dcd974db13d144de91
cf-gf-lc <rohanaggarwal090204@gmail.com>
2025-02-21 06:24:52 +0530

    Time: 31 ms (85.72%), Space: 97.7 MB (97.43%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -1,45 +1,43 @@
 class Solution {
 public:
-    vector<vector<int>> highestPeak(vector<vector<int>>& mat) {
-        for(int i=0;i<mat.size();i++){
-            for(int j=0;j<mat[0].size();j++){
-                mat[i][j]=mat[i][j]^1;
-            }
-        }
-        int n=mat.size();
-        int m=mat[0].size();
+    vector<vector<int>> highestPeak(vector<vector<int>>& matrices) {
+        int n=matrices.size();
+        int m=matrices[0].size();
+        vector<vector<int>>ans(n,vector<int>(m,10000));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(mat[i][j]==0){
+                if(matrices[i][j]==1){
+                    ans[i][j]=0;
                     continue;
                 }
                 int top=10000;
                 int bottom=10000;
                 if(i-1>=0){
-                    top=mat[i-1][j];
+                    top=ans[i-1][j];
                 }
                 if(j-1>=0){
-                    bottom=mat[i][j-1];
+                    bottom=ans[i][j-1];
                 }
-                mat[i][j]=min(top,bottom)+1;
+                ans[i][j]=min(top,bottom)+1;
             }
         }
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
-                if(mat[i][j]==0){
+                if(matrices[i][j]==1){
+                    ans[i][j]=0;
                     continue;
                 }
                 int top=10000;
                 int bottom=10000;
                 if(i+1<n){
-                    top=mat[i+1][j];
+                    top=ans[i+1][j];
                 }
                 if(j+1<m){
-                    bottom=mat[i][j+1];
+                    bottom=ans[i][j+1];
                 }
-                mat[i][j]=min(mat[i][j],min(top,bottom)+1);
+                ans[i][j]=min(ans[i][j],min(top,bottom)+1);
             }
         }
-        return mat;
+        return ans;
     }
 };
