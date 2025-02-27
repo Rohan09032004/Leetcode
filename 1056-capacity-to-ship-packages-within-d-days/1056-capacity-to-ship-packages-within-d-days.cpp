62ed8c007f48317c5f444f9cbce369d76f8c0938
cf-gf-lc <rohanaggarwal090204@gmail.com>
2025-02-27 22:10:53 +0530

    Time: 7 ms (92.73%), Space: 34.9 MB (94.11%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+int solve(int n,int mid,vector<int>& weights){
+    int sum=0;
+    int days=1;
+    for(int i=0;i<n;i++){
+        if(weights[i]+sum<=mid){
+            sum+=weights[i];
+        }
+        else{
+            sum=weights[i];
+            days++;
+        }
+    }
+    return days;
+}
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n=weights.size();
+        int low=*max_element(weights.begin(),weights.end());
+        int high=0;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            high+=weights[i];
+        }
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(solve(n,mid,weights)<=days){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
