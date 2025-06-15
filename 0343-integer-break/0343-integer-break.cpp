022d138a807e7aacc5d501c50628516e710d5858
cf-gf-lc <rohanaggarwal090204@gmail.com>
2025-06-15 16:15:31 +0530

    Time: 0 ms (100%), Space: 7.9 MB (79.37%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        int ans=1;
+        if(n==2){
+            return 1;
+        }
+        else if(n==3){
+            return 2;
+        }
+        while(n>4){
+            ans*=3;
+            n-=3;
+        }
+        if(n>0){
+        return n*ans;
+        }
+        return ans;
+    }
+};
