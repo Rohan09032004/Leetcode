d150cd9e68e9ce6ce4685a5f7e290e4bc174339d
cf-gf-lc <rohanaggarwal090204@gmail.com>
2025-06-15 16:30:33 +0530

    Time: 0 ms (100%), Space: 7.8 MB (82.54%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int integerReplacement(int n) {
+        int count=0;
+        if(n==INT_MAX){
+            return 32;
+        }
+    while(n>1){
+           if(n%2==0){
+            n=n/2;
+            count++;
+        }
+        else{
+            if((n-1)%4==0 ||  n==3){
+                n=n-1;
+                count++;
+            }
+            else{
+                n=n+1;
+                count++;
+            }
+        }
+    }
+    return count;
+    }
+};
