45c3e8cce361e710ed070ebcb1b0d6cc5dd93e90
cf-gf-lc <rohanaggarwal090204@gmail.com>
2025-06-24 01:46:07 +0530

    Time: 971 ms (69.31%), Space: 329.2 MB (65.61%) - LeetHub

--- 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.cpp
+++ 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int subarrayBitwiseORs(vector<int>& a) {
     unordered_set<int> all;
-    vector<int> cur, nxt;
+    set<int> cur, nxt;
 
     for (int x : a) {
         nxt.clear();
-        nxt.push_back(x);
+        nxt.insert(x);
         for (int v : cur) {
-            int nv = v | x;
-            if (nxt.back() != nv) {
-                nxt.push_back(nv);
-            }
+        int nv = v | x;
+        nxt.insert(nv);
         }
-        cur.swap(nxt);
+        cur=nxt;
         for (int v : cur) {
             all.insert(v);
         }
