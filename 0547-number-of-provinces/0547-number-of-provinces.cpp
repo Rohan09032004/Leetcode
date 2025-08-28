2948877bb5f1e51b6e5408cb8d9659d347dd29ad
Rohan09032004 <rohanaggarwal090204@gmail.com>
2025-08-28 23:34:34 +0530

    Time: 14 ms (5.8%), Space: 21.1 MB (17.85%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-void solve(int node,vector<vector<int>>& isconnected,vector<bool>&visited){
-    visited[node]=1;
-    for(int i=0;i<isconnected.size();i++){
-        if(i!=node && !visited[i]&& isconnected[node][i]==1){
-            solve(i,isconnected,visited);
+void dfs(int nodel, vector<bool>&visit,vector<vector<int>>&adjl){
+    visit[nodel]=1;
+    for(int i=0;i<adjl[nodel].size();i++){
+        if(!visit[adjl[nodel][i]]){
+            dfs(adjl[nodel][i],visit,adjl);
         }
     }
+    return;
 }
-    int findCircleNum(vector<vector<int>>& isconnected) {
-        int n=isconnected.size();
-        vector<bool>visited(n,0);
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        vector<vector<int>>adjl(n);
+        vector<bool>visit(n,0);
         int ans=0;
         for(int i=0;i<n;i++){
-            if(!visited[i]){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
+                    adjl[i].push_back(j);
+                    adjl[j].push_back(i);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(!visit[i]){
                 ans++;
-                solve(i,isconnected,visited);
+                dfs(i,visit,adjl);
             }
         }
         return ans;
