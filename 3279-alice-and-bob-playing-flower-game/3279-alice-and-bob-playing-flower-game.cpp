4565ceb334c8b343334353e25c558d4a389e2e3c
Rohan09032004 <rohanaggarwal090204@gmail.com>
2025-08-29 13:11:33 +0530

    Time: 0 ms (100%), Space: 8.5 MB (48.89%) - LeetHub

--- 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     long long flowerGame(int n, int m) {
-        long long int ans=0;
-        for(int i=1;i<=n;i++){
-            if(i%2){
-                ans=ans+(m/2);
-            }
-            else{
-                ans=ans+((m+1)/2);
-            }
-            cout<<ans<<" ";
-        }
-        return ans;
+        return (long long)m*(long long)n/2;
     }
 };
