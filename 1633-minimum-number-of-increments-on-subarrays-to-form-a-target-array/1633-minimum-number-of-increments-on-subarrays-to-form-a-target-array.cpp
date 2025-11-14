7b32f4e658a4befde8cc6fa4cc6dc737f85648e3
Rohan09032004 <rohanaggarwal090204@gmail.com>
2025-11-14 19:07:32 +0530

    Time: 6 ms (11.08%), Space: 81.4 MB (5.05%) - LeetHub

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -6,18 +6,15 @@ public:
         stack<int>s;
         int min1=0;
         for(int i=0;i<n;i++){ 
-           if(s.empty()||target[i]>s.top()){
+        if(s.empty()||target[i]>s.top()){
             if(!s.empty()){   
             ans+=target[i]-s.top();
             }
             else{
                 ans+=target[i];
             }
+        }
             s.push(target[i]);
-           }
-           if(target[i]<s.top()){
-            s.push(target[i]);
-           }
         }
         return ans;
     }
